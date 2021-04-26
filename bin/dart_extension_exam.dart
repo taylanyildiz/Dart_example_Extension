0a6927da76c71f818026a301f3b2b076ace18ab2
Taylan YILDIZ <taylanyildz@outlook.com>
2021-04-26 10:22:21 +0300

    added iterable

--- bin/dart_extension_exam.dart
+++ bin/dart_extension_exam.dart
@@ -25,15 +25,35 @@ extension RangeAdded<T> on List<T> {
       ];
 }
 
+extension Rnage on int {
+  List<int> toIncluding(int max, {int step = 1}) =>
+      [for (int i = this; i <= max; i += step) i];
+}
+
+class InclusiveRange with IterableMixin<int> {
+  late Iterable<int> _value;
+  InclusiveRange(int first, [int? second, int? thrid]) {
+    if ((second ?? thrid) == null)
+      _value = 0.toIncluding(first);
+    else if (second != null)
+      _value = first.toIncluding(second, step: thrid ?? 1);
+  }
+  @override
+  Iterator<int> get iterator => _value.iterator;
+}
+
 void main(List<String> args) {
   // var list = UtilList.addedList([12, 44, 12, 44, 3, 3, 3, 2]);
   // var list2 = UtilList.addedList(['a', 'a', 'b', 'c', 'c']);
 
-  // /// [UtilList] out [12, 44, 3, 2]
+  // [UtilList] out [12, 44, 3, 2]
   // print(list);
   // print(list2);
 
   /// [Extension]
   var range = [12, 12, 33, 12, 3, 3, 2].added();
   print(range);
+
+  final createRange = InclusiveRange(10, 20, 2);
+  print(createRange);
 }
